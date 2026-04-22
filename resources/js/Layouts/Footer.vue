50180387b9b85f085adc38c5d94bd460de1f374c
Adri <adrianroscue@gmail.com>
2026-04-22 18:29:33 +0200

    Prueba de CD/CI

--- resources/js/Layouts/Footer.vue
+++ resources/js/Layouts/Footer.vue
@@ -2,7 +2,7 @@
     <footer class="tw:mt-auto pt-3 pb-3 container-fluid tw:bg-indigo-500 justify-content-center d-flex flex-column tw:w-full">
         <div class="d-flex text-center tw:my-3">
             <p class="mx-auto tw:text-white mb-0">
-                &copy; 2026 <span class="quantico-regular">SATAllite</span> v1.0
+                &copy; 2026 <span class="quantico-regular">SATAllite</span> v1.1
             </p>
         </div>
     </footer>
